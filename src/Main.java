9e11fb91977c84fec48b81d0acb7775d72bdf3d3
Filipp Voronov <fivoronov@gmail.com>
2022-05-21 17:31:55 +0300

    YearlyReport added, monthly report read

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,23 @@ import java.nio.file.Path;
 public class Main {
 
     public static void main(String[] args) {
-        
+        SimpleFileReader reader = new SimpleFileReader();
+
+//        System.out.println("Hello\nWorld!");
+
+        String content = reader.readFileContentsOrNull("resources/m.202101.csv");
+        String[] lines = content.split("\n"); // [строка1, строка2, ...]
+        for (int i = 1; i < lines.length; i++) {
+            String line = lines[i];
+            String[] parts = line.split(",");
+            String itemName = parts[0];
+            boolean isExpense = Boolean.parseBoolean(parts[1]); // "true" -> true
+            int quantity = Integer.parseInt(parts[2]);
+            int sumOfOne = Integer.parseInt(parts[3]);
+
+            MonthlyReportRecord record = new MonthlyReportRecord(itemName, isExpense, quantity, sumOfOne);
+            monthlyRecord.add(record);
+        }
     }
 
 }
---
+++ src/YearlyReport.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class YearlyReport {
+    ArrayList<YearlyReportRecord> records = new ArrayList<>();
+
+    public void add(YearlyReportRecord record) {
+        records.add(record);
+    }
+}
